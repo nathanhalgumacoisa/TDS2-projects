31873cff23b823a0d29d4db744ca1c988ca43958
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-08-08 14:10:48 -0300

    new feat: create savePost and cleanFields functions

---
+++ assets/js/script.post.js
@@ -0,0 +1,15 @@
+function savePost(){
+    const title = document.getElementById("title").value
+    const resume = document.getElementById("resume").value
+    const category = document.getElementById("category").value
+    const author = document.getElementById("author").value
+    const date = document.getElementById("date").value
+}
+
+function cleanField(){
+    document.getElementById("title").value
+    document.getElementById("resume").value
+    document.getElementById("category").value
+    document.getElementById("author").value
+    document.getElementById("date").value
+}
