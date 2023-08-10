df06d0af1255d0a5c548896b6602fd00e79d6ca4
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-08-10 11:06:43 -0300

    new feat:putting the imc calculator project

--- assets/js/teste.js
+++ assets/js/teste.js
@@ -32,7 +32,11 @@ class User {
 }
 
 const user1 = new User('João', 33);
-user1.introduce()*/
+user1.introduce()
+
+
+
+
 class Pizza {
     constructor(base, sauce, toppings, cheese) {
         this.base = base;
@@ -89,3 +93,72 @@ const myPizza = new Pizza('integral', 'tomato', 'pepperoni', 'muzzarela'  )
 console.log(myPizza.getDescription())
 let getTimerTotal =  myPizza.getTimerBase() + myPizza.getTimerSauce() + myPizza.getTimerToppings() + myPizza.getTimerCheese() 
 console.log(`Time to cook the pizza =  ${getTimerTotal}`)
+
+
+
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+    multiply(a, b){
+        return a * b;
+    }
+
+    substract(a, b){
+        return a - b
+    }
+}
+
+const calculator = new Calculator();
+
+const sumResult = calculator.add(5, 3);
+console.log("sum:", sumResult);
+
+const multiplicationResult = calculator.multiply(4, 2);
+console.log("multiplication:", multiplicationResult);
+
+const substractionResult = calculator.substract(10, 4);
+console.log("substraction", substractionResult);*/
+
+class Witch{
+    constructor(name, patron, house, colorEyes){
+        this.name = name;
+        this.patron = patron;
+        this.house = house;
+        this.colorEyes = colorEyes;
+    }
+
+    getName(){
+        return `O nome do bruxo(a) é: ${this.name}`;
+    }
+    getHouse(){
+        return `Ela(a) está na casa: ${this.house}`;
+    }
+    getPatron(){
+        return `Seu patrono é: ${this.patron}`
+    }
+}
+
+const Witch1 = new Witch('Harry Potter', 'Cervo', 'Grifinória', 'Verde/Azul')
+console.log(Witch1.getName())
+console.log(Witch1.getHouse())
+console.log(Witch1.getPatron())
+
+class TypeWitch extends Witch {
+    constructor(name, patron, house, colorEyes, TypeWitch) {
+        super(name, patron, house, colorEyes);
+        this.TypeWitch = TypeWitch;
+    }
+    getTypeWitch() {
+        if(this.TypeWitch === 'P'){
+            return "Este bruxo é sangue puro";
+        }else if(this.TypeWitch === 'M'){
+            return "Este bruxo é mestiço";
+        }else{
+            return "Este é um trouxa";
+        }
+    }
+}
+
+const Harry = new TypeWitch('Harry Potter', 'Cervo', 'Grifinória', 'Verde/Azul', 'M');
+console.log(Harry.getTypeWitch())
