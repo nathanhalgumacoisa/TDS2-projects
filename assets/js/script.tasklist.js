10f62f8ff4e8833c52039851d2fd934b0414bc23
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-08-15 17:12:32 -0300

    sad

--- assets/js/script.tasklist.js
+++ assets/js/script.tasklist.js
@@ -9,14 +9,39 @@ class TaskList {
     constructorTL(){
         this.tasks = []
     }
-    addPost(tasks)
+    addPost(tasks){
+        this.tasks.push(Task)
+    }
+    deletePost(id){
+        this
+    }
 }
 
 function randomTask(){
     let id = Math.floor(Math.random() * 9999)
 }
+const taskList = new TaskList()
 function taskList(){
     let title = document.getElementById("addTask").value
     const newTask = new Task(randomTask(), false, title);
-    taskList..addPost(newTask)
+    taskList.addPost(newTask)
+}
+function cleanField(){
+    document.getElementById("addTask").value = ""
+}
+function showContent(){
+document.getElementById("taskroom").classList.remove("hidden")
+let showTask = ""
+taskList.tasks.forEach((newTask, index) => {
+    showTask += `
+        <div class="postTask">
+            <h2>${TaskList.title}</h2>
+            <div id="buttons">
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+        </div>
+    `
+});
 }
