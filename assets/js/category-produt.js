8753e04cb1347b5f58c3374e0a6b46b9e2cfd0b5
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-08-17 10:15:22 -0300

    new feat: creation of the class Category

---
+++ assets/js/category-produt.js
@@ -0,0 +1,9 @@
+//creation of the class Category
+class Category {
+    constructor(id, name){
+        this.id = id
+        this.name = name
+        //array products
+        this.products = []
+    }
+}
