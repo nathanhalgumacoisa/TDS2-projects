294519d42a7509dc9b5996f1e327b77e6e330de0
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-08-17 10:19:22 -0300

    new feat: class product

--- assets/js/category-produt.js
+++ assets/js/category-produt.js
@@ -7,3 +7,12 @@ class Category {
         this.products = []
     }
 }
+//product class
+class product {
+    constructor(id, name, price, category){
+        this.id = id;
+        this.name = name;
+        this.price = price
+        this.category = category
+    }
+}
