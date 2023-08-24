b10e777c63c80ff761afe601ead41c0053b72453
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-08-24 15:36:36 -0300

    class Carro

---
+++ aulas/assets/js/aluguelcarro.js
@@ -0,0 +1,11 @@
+class Carro{
+    constructor(marca, modelo, ano, cor, km, preco, imagem){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+        this.km = km;
+        this.preco = preco;
+        this.imagem = imagem;
+    }
+}
